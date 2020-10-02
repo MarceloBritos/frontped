790cbabe00a8a75ebf5a4ae6bc2e2486274a616f
MarceloBritos <marcelobd07@gmail.com>
2020-10-02 12:42:37 -0300

    Updates

---
+++ precache-manifest.3b09e4706214dac433a6ec6895dbac55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2281178cdd1bca1a1dddd738c6aebc61",
+    "url": "/MarceloBritos/frontped/index.html"
+  },
+  {
+    "revision": "32710cdc16ee5b7151d0",
+    "url": "/MarceloBritos/frontped/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5000d784d5bfa50689e3",
+    "url": "/MarceloBritos/frontped/static/js/2.69e2be96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MarceloBritos/frontped/static/js/2.69e2be96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32710cdc16ee5b7151d0",
+    "url": "/MarceloBritos/frontped/static/js/main.62dca55a.chunk.js"
+  },
+  {
+    "revision": "57bb17d9d44daa94f8cb",
+    "url": "/MarceloBritos/frontped/static/js/runtime-main.c21f3025.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MarceloBritos/frontped/static/media/logo.5d5d9eef.svg"
+  }
+]);
